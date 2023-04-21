49a61fd33dfd8b29f12b6e071bc89c3e32979af6
Allison Lowery <a.lowery@setonhill.edu>
2023-04-21 14:22:09 -0400

    Finished

--- Homework7-Gestures/ViewController.swift
+++ Homework7-Gestures/ViewController.swift
@@ -9,6 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
+    @IBAction func foundTap(_ sender: Any) {
+        
+        outputLabel.text = "You Tapped!\nChoose Another!!"
+    }
+    
+    
+    
+    
+    @IBAction func foundSwipe(_ sender: Any) {
+        
+        outputLabel.text = "You Swiped!\nTry Another One!!"
+    }
+    
+    
+    
+    
+    @IBAction func foundRotate(_ sender: Any) {
+        
+        outputLabel.text = "You Rotated!\nPick Another!!"
+    }
+    
+    
+    
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        if motion==UIEvent.EventSubtype.motionShake {
+            outputLabel.text = "Shaking Things Up!"
+        }
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
