c6baf5173811f6c16d4bf95ce0e92ff937e88ca0
Allison Lowery <a.lowery@setonhill.edu>
2023-04-21 12:41:04 -0400

    Initial Commit

---
+++ Homework7-Gestures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework7-Gestures
+//
+//  Created by Allie Lowery on 4/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
